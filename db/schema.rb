ceb41d0999d1202b723d845cf6a85b5db0244d60
Marcel <malbrecht0792@gmail.com>
2018-03-25 10:28:25 -0400

    Implemented Post model using TDD

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180325135549) do
+ActiveRecord::Schema.define(version: 20180325142127) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title"
+    t.text     "article"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
