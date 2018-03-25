2806390b748653592897ab596d6fa6dd42d60223
Marcel <malbrecht0792@gmail.com>
2018-03-25 12:56:43 -0400

    Implemented Post association using TDD

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,15 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  it 'saves a post with valid input' do
-    expect(build(:post)).to be_valid
+  describe('Validations') do
+    let(:user) {User.new(username: 'johndoe', email: 'johndoe@example.com')}
+    subject {
+      described_class.new(title: 'My Post', article: 'My first post', user: user)
+    }
+
+    it 'saves a post with valid input' do
+      expect(subject).to be_valid
+    end
+
+    it 'does not save a post without a title' do
+      expect(build(:invalid_title)).not_to be_valid
+    end
+
+    it 'does not save a post without an article' do
+      expect(build(:invalid_article)).not_to be_valid
+    end
   end
 
-  it 'does not save a post without a title' do
-    expect(build(:invalid_title)).not_to be_valid
-  end
-
-  it 'does not save a post without an article' do
-    expect(build(:invalid_article)).not_to be_valid
+  describe('Associations') do
+    it { should belong_to(:user) }
   end
 end
